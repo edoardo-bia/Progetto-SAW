c3885ca15e28d341bc39c9e1c05219a860849938
Edoardo <edoardobiagioni@gmail.com>
2024-07-26 00:36:01 +0200

    Prima implementazione navbar

---
+++ src/navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+
+function Navbar(){
+    return(
+        <>
+            <ul id="container_nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </>
+    )
+}
+
+
+export default Navbar
