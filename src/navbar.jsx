099ebd26550703064e595f3818661efaa1b6727d
Edoardo <edoardobiagioni@gmail.com>
2024-07-26 10:50:12 +0200

    aggiornamento navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,11 +3,19 @@ import "./Navbar.css"
 function Navbar(){
     return(
         <>
-            <ul id="container_nav">
+        <div>
+            <ul id="link_nav">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Contacts</a></li>
             </ul>
+        </div>
+        <div>
+        <ul id="icon_nav">
+                <li onClick={() => handleClick('theme')} style={{cursor: 'pointer'}}> <img src="./src/assets/theme.png" width={30} height={30} alt="Theme icon" /> </li>
+                <li onClick={() => handleClick('login')} style={{cursor: 'pointer'}}> <img src="./src/assets/login.png" width={30} height={30} alt="Theme icon" /> </li>
+            </ul>
+        </div>
         </>
     )
 }
