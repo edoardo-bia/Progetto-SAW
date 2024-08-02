f33149abcb1c68d3f2ea48d9c11d44bfd75a6961
Edoardo <edoardobiagioni@gmail.com>
2024-08-02 22:38:42 +0200

    Aggiornato Progetti e migliorato immagini

--- src/Project.jsx
+++ src/Project.jsx
@@ -14,13 +14,13 @@ function Project(){
                 <h3>Those are the project that I made:</h3>
             <div className='cards'>    
                 <div className="top-cards">
-                    <Card title="Lab 3" content="Java exam where i created a tripAdvisor like app" img="./src/assets/tripAdvisor.png" alt="app" onClick={() => handleCardClick("1")}/>
-                    <Card title="Mine Sweeper" content="I tried to recreate a mine sweepwe game uusing Java to exercise" img="./src/assets/mineSweeper.png" alt="app" onClick={() => handleCardClick("2")}/>
-                    <Card title="Lab 2" content="C Exam using threads" img="./src/assets/lab2.png" alt="app" onClick={() => handleCardClick("3")}/>
+                    <Card title="Lab 3" content="Java exam where i created a tripAdvisor like app (backend)" img="./src/assets/tripAdvisor.jpg" alt="app" onClick={() => handleCardClick("1")}/>
+                    <Card title="Mine Sweeper" content="I tried to recreate Mine sweeper using Java to exercise" img="./src/assets/mineSweeper.jpeg" alt="app" onClick={() => handleCardClick("2")}/>
+                    <Card title="Lab 2" content="Exam in C and Python using threads, developed in Linux" img="./src/assets/lab2.jpg" alt="app" onClick={() => handleCardClick("3")}/>
                 </div>
                 <div className="bottom-cards">    
-                    <Card title="Cybersecurity" content="Power point presentation about keyloggers" img="./src/assets/sicurezza2.png" alt="app" onClick={() => handleCardClick("4")}/>
-                    <Card title="This Web App" content="Created using react for SAW exam" img="./src/assets/thisApp.png" alt="app" onClick={() => handleCardClick("5")}/>
+                    <Card title="Cybersecurity" content="Power point presentation about malware and keyloggers" img="./src/assets/sicurezza2.jpg" alt="app" onClick={() => handleCardClick("4")}/>
+                    <Card title="This Web App" content="It's this site! Created using react for SAW exam" img="./src/assets/thisApp.jpg" alt="app" onClick={() => handleCardClick("5")}/>
                 </div>
             </div>   
         </>
