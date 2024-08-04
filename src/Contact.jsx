1eb942268b6356232f44483fd8ad288ad5a273b3
Edoardo <edoardobiagioni@gmail.com>
2024-08-05 00:12:50 +0200

    Risolto bug icone navbar

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -12,19 +12,19 @@ function Contacts(){
                 <ul className='ul_con'>
                     <li className='li_con'>
                         <a href="mailto: edoardobiagioni@gmail.com">
-                            <img src="./src/assets/email-icon.png" alt="Email Icon" class="icon_con"/>
+                            <img src="./src/assets/email-icon.png" alt="Email Icon" className="icon_con"/>
                             <span>Email</span>
                         </a>
                     </li>
                     <li className='li_con'>
                         <a href="https://www.linkedin.com/in/edoardobiagioni/" target="_blank">
-                            <img src="./src/assets/linkedin-icon.png" alt="LinkedIn Icon" class="icon_con"/>
+                            <img src="./src/assets/linkedin-icon.png" alt="LinkedIn Icon" className="icon_con"/>
                             <span>LinkedIn</span>
                         </a>
                     </li>
                     <li className='li_con'>
                         <a href="https://github.com/edoardo-bia" target="_blank">
-                            <img src="./src/assets/github-icon.png" alt="GitHub Icon" class="icon_con"/>
+                            <img src="./src/assets/github-icon.png" alt="GitHub Icon" className="icon_con"/>
                             <span>GitHub</span>
                         </a>
                     </li>
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Form from './form';
 import "./Navbar.css"
+import loginLogo from '../src/assets/login.png';
+import themeLogo from '../src/assets/theme.png';
 
 function Navbar() {
   
@@ -33,8 +35,8 @@ const [showLogin, setShowLogin] = useState(false);
         </div>
         <div>
           <ul id="icon_nav">
-            <li className='li_nav' onClick={() => handleClick('theme')}> <img src="./src/assets/theme.png" width={30} height={30} alt="Theme icon" /> </li>
-            <li className='li_nav' onClick={() => handleLoginClick('login')}> <img src="./src/assets/login.png" width={29} height={29} alt="Login icon" /> </li>
+            <li className='li_nav' onClick={() => handleClick('theme')}> <img src={themeLogo} width={30} height={30} alt="Theme icon" /> </li>
+            <li className='li_nav' onClick={() => handleLoginClick('login')}> <img src={loginLogo} width={29} height={29} alt="Login icon" /> </li>
             {showLogin && 
               <div id='login_form_container'>
                 <Form />
