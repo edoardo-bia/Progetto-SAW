1789a58a733afb3979fc7e0bf594d65c8f92a7f9
Edoardo Biagioni <123489417+edoardo-bia@users.noreply.github.com>
2024-08-08 11:00:55 +0200

    Update firebase.jsx

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -1,14 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiIb5OFVB0K7Q32H8EAmF2WcWef4jzb3Y",
-  authDomain: "progetto-saw-6e446.firebaseapp.com",
-  projectId: "progetto-saw-6e446",
-  storageBucket: "progetto-saw-6e446.appspot.com",
-  messagingSenderId: "715633570916",
-  appId: "1:715633570916:web:ff22cf1cf6514e0f8a9b71"
+  apiKey: "MYKEY",
+  authDomain: "MYDOMAIN",
+  projectId: "MYPROJECT",
+  storageBucket: "MYSTORAGE",
+  messagingSenderId: "MYSENDER",
+  appId: "MYAPP"
 };
 
 // Initialize Firebase
